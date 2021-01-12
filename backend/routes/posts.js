be7d521a1a53546a86bde8115ba5fb9eec88f26a
Yasiru Randika <43146170+YasiruRandika@users.noreply.github.com>
2021-01-12 16:34:20 +0530

    Add Controllers for Backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,8 +1,9 @@
-const { jitOnlyGuardedExpression } = require('@angular/compiler/src/render3/util');
 const express = require('express');
 const router = express.Router();
+const postController = require('../controllers/post');
 const multer = require('multer');
-const Post = require('../models/post');
+
+
 const authorization = require('../middleware/check-auth');
 
 const MIME_TYPE_MAP = {
@@ -30,113 +31,14 @@ const storage = multer.diskStorage({
   }
 });
 
-router.get('', (req, res, next) => {
-  const pageSize = +req.query.pageSize;
-  const currentPage = +req.query.page;
-  let fetchedPosts;
+router.get('', postController.getPosts);
 
-  const postQuery = Post.find();
+router.put('/:id', authorization, multer({storage : storage}).single("image"), postController.updatePost);
 
-  if(currentPage && pageSize) {
-    postQuery.skip(pageSize * (currentPage - 1))
-    .limit(pageSize);
-  }
+router.post("", authorization, multer({storage : storage}).single("image"), postController.createPost);
 
-  postQuery.then(documents => {
-    fetchedPosts = documents;
-    console.log(Post.count())
-    return Post.count();
-  })
-  .then(count => {
-    return res.status(200).json({
-      message:'Post fetched Successully',
-      posts:fetchedPosts,
-      maxPosts : count
-    });
-  });
-});
+router.delete("/:id", authorization, postController.deletePost);
 
-router.put('/:id', authorization, multer({storage : storage}).single("image"), (req, res, next) => {
-  let imagePath = req.body.imagePath;
-
-  if(req.file) {
-    const url = req.protocol + "://" + req.get("host");
-    imagePath = url +"/images/" + req.file.filename;
-  }
-  console.log(imagePath);
-
-  const post = new Post({
-    _id : req.body.id,
-    title : req.body.title,
-    content : req.body.content,
-    imagePath : imagePath,
-    creator : req.userData.userId
-  });
-
-  Post.updateOne({ _id: req.params.id, creator : req.userData.userId }, post).then(result => {
-    if(result.nModified > 0) {
-    res.status(200).json({
-      message: "Update successful!",
-      imagePath : result.imagePath
-  });
-} else {
-  res.status(401).json({
-    message:'Authorization Failed'
-  })
-}
-  });
-})
-
-router.post("", authorization, multer({storage : storage}).single("image"),(req, res, next) => {
-  const url = req.protocol + "://" + req.get("host");
-  const post = new Post({
-    title : req.body.title,
-    content : req.body.content,
-    imagePath : url +"/images/" + req.file.filename,
-    creator: req.userData.userId
-  });
-
-  post.save()
-  .then(result => {
-    console.log(post);
-  res.status(201).json({
-    message:"Post Added",
-    ...result,
-    postId:result._id,
-    imagePath : result.imagePath
-  });
-  });
-
-
-});
-
-router.delete("/:id", authorization, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator : req.userData.userId }).then(result => {
-    console.log(result);
-    if(result.n > 0) {
-    res.status(200).json({ message: "Post deleted!" });
-    } else {
-      res.status(401).json({ message: "Auth Failed" });
-    }
-  });
-});
-
-router.get("/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      console.log(post);
-      res.status(200).json(
-        {
-          _id : post._id,
-          title : post.title,
-          content : post.content,
-          imagePath : post.imagePath
-        }
-      );
-    } else {
-      res.status(404).json({ message: "Post not found!" });
-    }
-  });
-})
+router.get("/:id", postController.getPostById);
 
 module.exports = router;
